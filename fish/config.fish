c64597a520973559e63ea7b6c55e52448839c40a
diego-watanabe <diego.e.watanabe@gmail.com>
2024-05-28 03:57:28 -0400

    Initial commit

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+if type -q exa
+	alias ls='exa --color=always'
+else
+	alias ls='ls --color=auto'
+end
+ 
+set -x PATH $PATH /usr/bin
